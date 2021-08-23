639ee856a643f926c94820df091634c2951dd18f
Jose Daniel Flores Rodriguez <37556693+dudeflores@users.noreply.github.com>
2021-08-23 21:09:41 +0100

    Add files via upload

---
+++ JavaBeans/src/test/TestJavaBeans.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Persona;
+
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Jose");
+        persona.setApellido("Flores");
+        
+        System.out.println("persona = " + persona);
+        
+        System.out.println("Persona nombre: " +persona.getNombre());
+        System.out.println("Persona apellido: " +persona.getApellido());
+    }
+}
